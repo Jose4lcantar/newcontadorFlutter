c49baa0b27d1de364af293bf01f545462358d459
Jose <joseam.ti22@utsjr.eu.mx>
2024-02-15 23:11:41 -0600

    first commit

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -8,7 +8,6 @@ class HomePage extends StatefulWidget {
 }
 class _HomePageState extends State<HomePage> {
   int cont = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
